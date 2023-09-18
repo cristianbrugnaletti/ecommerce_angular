0d1ebd9d1e0699933122cddb1939ffdb610a5376
cristianbrugnaletti <c.brugnaletti@outlook.com>
2023-09-18 17:33:41 +0200

    progetto inziiale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home-component.component';
+import { FornitoriComponent } from './Fornitore/fornitori/fornitori-component.component';
+import { AggiungiFornitoreComponent } from './Fornitore/aggiungi-fornitore/aggiungi-fornitore.component';
+import { ClienteComponent } from './Cliente/cliente/cliente-component.component';
+import { AggiungiClienteComponent } from './Cliente/aggiungi-cliente/aggiungi-cliente.component';
+import { ProdottiComponent } from './Prodotto/prodotti/prodotti-component.component';
+import { AggiungiProdottoComponent } from './Prodotto/aggiungi-prodotto/aggiungi-prodotto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'fornitori', component: FornitoriComponent },
+  { path: 'aggiungi-fornitore', component: AggiungiFornitoreComponent },
+  { path: 'clienti', component: ClienteComponent },
+  { path: 'aggiungi-cliente', component: AggiungiClienteComponent },
+  { path: 'prodotti', component: ProdottiComponent },
+  { path: 'aggiungi-prodotto', component: AggiungiProdottoComponent },
+ 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { PanelMenuModule } from 'primeng/panelmenu';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home-component.component';
+import { FornitoriComponent } from './Fornitore/fornitori/fornitori-component.component';
+import { AggiungiFornitoreComponent } from './Fornitore/aggiungi-fornitore/aggiungi-fornitore.component';
+import { ClienteComponent } from './Cliente/cliente/cliente-component.component';
+import { AggiungiClienteComponent } from './Cliente/aggiungi-cliente/aggiungi-cliente.component';
+import { ProdottiComponent } from './Prodotto/prodotti/prodotti-component.component';
+import { AggiungiProdottoComponent } from './Prodotto/aggiungi-prodotto/aggiungi-prodotto.component';
+import { MenuComponent } from './home/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FornitoriComponent,
+    AggiungiFornitoreComponent,
+    ClienteComponent,
+    AggiungiClienteComponent,
+    ProdottiComponent,
+    AggiungiProdottoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MenubarModule,
+    ButtonModule,
+    TieredMenuModule,
+    PanelMenuModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
