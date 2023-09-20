560037678ae6fcdfc2932806ddb8f0b527172cbf
tonitoni88 <a.aloia@tdnet.it>
2023-09-20 15:09:20 +0200

    conferma aggiunta magazzino

--- src/app/Magazzino/aggiungi-magazzino/aggiungi-magazzino.component.spec.ts
+++ src/app/Magazzino/aggiungi-magazzino/aggiungi-magazzino.component.spec.ts
@@ -1,21 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { MagazzinoDTO } from '../MagazzinoDTO';
+import { MagazzinoService } from '../magazzino.service';
 
-import { AggiungiMagazzinoComponent } from './aggiungi-magazzino.component';
+@Component({
+  selector: 'app-aggiungi-magazzino',
+  templateUrl: './aggiungi-magazzino.component.html',
+  styleUrls: ['./aggiungi-magazzino.component.css'],
+})
+export class AggiungiMagazzinoComponent {
+  magazzino: MagazzinoDTO = new MagazzinoDTO();
+  successMessage: string | null = null; // Inizializza a null
+  errorMessage: string | null = null; // Inizializza a null
 
-describe('AggiungiMagazzinoComponent', () => {
-  let component: AggiungiMagazzinoComponent;
-  let fixture: ComponentFixture<AggiungiMagazzinoComponent>;
+  constructor(private magazzinoService: MagazzinoService) {}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AggiungiMagazzinoComponent]
-    });
-    fixture = TestBed.createComponent(AggiungiMagazzinoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  aggiungiMagazzino() {
+    this.magazzinoService.aggiungiMagazzino(this.magazzino).subscribe(
+      (response) => {
+        // Gestisci la risposta del server
+        console.log('Risposta del server:', response);
+        if (response.message) {
+          // Se la risposta contiene un messaggio, mostra il messaggio di successo
+          this.successMessage = response.message;
+          this.errorMessage = null; // Assicurati che il messaggio di errore sia vuoto
+        } else {
+          console.error('Risposta del server sconosciuta:', response);
+        }
+      },
+      (error) => {
+        console.error('Si è verificato un errore durante l\'aggiunta del magazzino:', error);
+        // Gestisci l'errore, ad esempio, mostra un messaggio di errore all'utente
+        this.errorMessage = 'Errore durante l\'aggiunta del magazzino.';
+        this.successMessage = null; // Assicurati che il messaggio di successo sia vuoto
+      }
+    );
+  }
+}
--- src/app/Magazzino/aggiungi-magazzino/aggiungi-magazzino.component.ts
+++ src/app/Magazzino/aggiungi-magazzino/aggiungi-magazzino.component.ts
@@ -3,7 +3,6 @@ import { MagazzinoDTO } from '../../MagazzinoDTO';
 import { MagazzinoService } from '../../magazzino.service';
 import { CommonModule } from '@angular/common'; // Importa CommonModule
 import { FormsModule } from '@angular/forms'; // Importa FormsModule per ngModel
-
 @Component({
   selector: 'app-aggiungi-magazzino',
   templateUrl: './aggiungi-magazzino.component.html',
@@ -11,17 +10,29 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule per ngModel
 })
 export class AggiungiMagazzinoComponent {
   magazzino: MagazzinoDTO = new MagazzinoDTO();
+  successMessage: string | null = null; // Inizializza a null
+  errorMessage: string | null = null; // Inizializza a null
 
   constructor(private magazzinoService: MagazzinoService) {}
 
   aggiungiMagazzino() {
     this.magazzinoService.aggiungiMagazzino(this.magazzino).subscribe(
       (response) => {
-        // Gestisci la risposta dal backend, ad esempio, mostra un messaggio di successo o reindirizza l'utente
+        // Gestisci la risposta del server
+        console.log('Risposta del server:', response);
+        if (response.message) {
+          // Se la risposta contiene un messaggio, mostra il messaggio di successo
+          this.successMessage = response.message;
+          this.errorMessage = null; // Assicurati che il messaggio di errore sia vuoto
+        } else {
+          console.error('Risposta del server sconosciuta:', response);
+        }
       },
       (error) => {
         console.error('Si è verificato un errore durante l\'aggiunta del magazzino:', error);
         // Gestisci l'errore, ad esempio, mostra un messaggio di errore all'utente
+        this.errorMessage = 'Errore durante l\'aggiunta del magazzino.';
+        this.successMessage = null; // Assicurati che il messaggio di successo sia vuoto
       }
     );
   }
