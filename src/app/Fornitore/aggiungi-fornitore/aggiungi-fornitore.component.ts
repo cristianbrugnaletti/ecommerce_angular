7ad3ca815dc204d65201ba78a086bd32c2d693f5
cristianbrugnaletti <c.brugnaletti@outlook.com>
2023-09-20 17:58:06 +0200

    messaggi fornitore

--- src/app/Fornitore/aggiungi-fornitore/aggiungi-fornitore.component.ts
+++ src/app/Fornitore/aggiungi-fornitore/aggiungi-fornitore.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { FornitoreService } from '../fornitori/fornitore.service';
-import { FornitoreDTO } from '../fornitori/fornitoreDTO'; // Assicurati di importare il servizio corretto
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FornitoreDTO } from '../fornitori/fornitoreDTO';
+import { Message } from 'primeng/api'; // Assicurati di importare 'Message' da primeng
 
 @Component({
   selector: 'app-aggiungi-fornitore',
@@ -10,7 +9,9 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./aggiungi-fornitore.component.css'],
 })
 export class AggiungiFornitoreComponent {
-  fornitore: FornitoreDTO = new FornitoreDTO(); // Assicurati di utilizzare il DTO corretto
+  fornitore: FornitoreDTO = new FornitoreDTO();
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
 
   constructor(private fornitoreService: FornitoreService) {}
 
@@ -18,10 +19,18 @@ export class AggiungiFornitoreComponent {
     this.fornitoreService.aggiungiFornitore(this.fornitore).subscribe(
       (response) => {
         // Gestisci la risposta dal backend, ad esempio, mostra un messaggio di successo o reindirizza l'utente
+        if (response && response.message) {
+          this.successMessage = response.message;
+          this.errorMessage = null;
+        } else {
+          console.error('Risposta del server sconosciuta:', response);
+        }
       },
       (error) => {
         console.error('Si è verificato un errore durante l\'aggiunta del fornitore:', error);
         // Gestisci l'errore, ad esempio, mostra un messaggio di errore all'utente
+        this.errorMessage = 'Si è verificato un errore durante l\'aggiunta del fornitore.';
+        this.successMessage = null;
       }
     );
   }
