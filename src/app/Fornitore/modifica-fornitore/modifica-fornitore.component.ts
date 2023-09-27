0d631bc84fa36879c112fecb6f2a93e49231fdd9
cristianbrugnaletti <c.brugnaletti@outlook.com>
2023-09-27 12:19:54 +0200

    modifica ed annulla di fornitore

--- src/app/Fornitore/modifica-fornitore/modifica-fornitore.component.ts
+++ src/app/Fornitore/modifica-fornitore/modifica-fornitore.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FornitoreDTO } from '../fornitori/fornitoreDTO';
 import { FornitoreService } from '../fornitori/fornitore.service';
-import { ActivatedRoute, Router } from '@angular/router'; // Importa ActivatedRoute e Router
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-modifica-fornitore',
@@ -10,43 +10,54 @@ import { ActivatedRoute, Router } from '@angular/router'; // Importa ActivatedRo
 })
 export class ModificaFornitoreComponent {
   @Input() fornitore: FornitoreDTO;
-  @Output() modificaCompletata = new EventEmitter<void>();
+  @Output() modificaCompletata: EventEmitter<void> = new EventEmitter<void>();
+
   erroreModifica: string = '';
-  
   partitaIVA: string = '';
-  
+  fornitoreInModifica: FornitoreDTO | null = null;
   constructor(
     private fornitoreService: FornitoreService,
-    private router: Router, // Inietta il servizio Router
-    private route: ActivatedRoute // Inietta ActivatedRoute nel costruttore
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.fornitore = {} as FornitoreDTO;
     this.partitaIVA = '';
     this.route.params.subscribe(params => {
-      this.partitaIVA = params['partitaIVA']; // Utilizza 'partitaIVA' invece di 'id'
+      this.partitaIVA = params['partitaIVA'];
     });
   }
 
+  inModifica: boolean = true;
+  modificaInCorso: boolean = false;
+
   confermaModifica(event: Event) {
-    event.preventDefault(); // Impedisce il comportamento predefinito del form
-  
+    event.preventDefault();
+
     if (this.fornitore && this.fornitore.partitaIVA) {
-      this.erroreModifica = ''; // Resetta eventuali errori precedenti
-      console.log('Dati da inviare nella richiesta PUT:', this.fornitore);
+      this.erroreModifica = '';
+      this.modificaInCorso = true;
+
       this.fornitoreService.modificaFornitore(this.fornitore.partitaIVA, this.fornitore).subscribe(
         () => {
-          // La modifica è completata con successo
-          this.modificaCompletata.emit(); // Prima emetti l'evento
-          this.router.navigate(['/home']); // Poi esegui il reindirizzamento
+          this.modificaInCorso = false;
+          console.log('Reindirizzamento a /fornitori');
+          this.modificaCompletata.emit();
+          this.router.navigate(['/fornitori']);
         },
         (errore) => {
-          // Gestisci eventuali errori qui e mostra un messaggio di errore all'utente
           this.erroreModifica = 'Si è verificato un errore durante la modifica del fornitore.';
           console.error('Errore durante la modifica del fornitore:', errore);
+          this.modificaInCorso = false;
         }
       );
     } else {
       this.erroreModifica = 'Partita IVA non definita.';
     }
+  }
+
+  annullaModifica() {
+   
+    window.location.href = '/fornitori';
+    console.log('Reindirizzamento a /fornitori');
   }
 }
